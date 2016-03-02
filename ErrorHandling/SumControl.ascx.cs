2f1c8395ecbfd5a5770d1e0d9cf9f3cc24f16285
lixiaolei19821218 <735534185@qq.com>
2016-03-02 17:34:19 +0800

    add

---
+++ ErrorHandling/SumControl.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ErrorHandling
+{
+    public partial class SumControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                int first = int.Parse(Request.Form["first"]);
+                int second = int.Parse(Request.Form["second"]);
+                result.InnerText = (first + second).ToString();
+                resultPlaceholder.Visible = true;
+            }
+        }
+    }
+}
